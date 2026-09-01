3251d4260334450cfdebe06754ddaa0fbe23097d
Kevin Walker <walkk245.208@lns.org.uk>
2026-09-01 15:57:19 +0100

    Updated database

--- server/app.py
+++ server/app.py
@@ -4,13 +4,22 @@ import sqlite3
 app = Flask(__name__)
 
 # Route to display the form
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 @app.route('/user-form')
 def user_form():
     return render_template('user-form.html')
+
+@app.route('/order')
+def order_form():
+    return render_template('order.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
 
 
 # Route to handle form submission (POST)
@@ -73,3 +82,56 @@ def orders():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Route to handle form submission (POST)
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        # Get form data
+        name = request.form['name']
+        email = request.form['email']
+        city = request.form['city']
+        phone = request.form['phone']
+
+        try:
+            # Connect to database
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+
+            # Insert data into table
+            cursor.execute('''INSERT INTO users (name, email, city, phone) 
+                            VALUES (?, ?, ?, ?)''', 
+                          (name, email, city, phone))
+
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('success'))
+        except Exception as e:
+            return f"Error: {e}"
+
+# Route to handle form submission (POST)
+@app.route('/submit-orders', methods=['POST'])
+def submit_orders():
+    if request.method == 'POST':
+        # Get form data
+        order_type = request.form['order_type']
+        created = request.form['created']
+        #total_price = request.form['']
+        
+        try:
+            # Connect to database
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+
+            # Insert data into table
+            cursor.execute('''INSERT INTO users (order_type, created) 
+                            VALUES (?, ?, ?, ?)''', 
+                          (order_type, created))
+
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('success'))
+        except Exception as e:
+            return f"Error: {e}"
